a90c05da8771d76ded6e74b2e36598faa27a6d0f
turpure <p812816627@163.com>
2017-10-16 11:37:55 +0800

    modify view of oa-goods index

--- advanced/backend/views/oa-goods/index.php
+++ advanced/backend/views/oa-goods/index.php
@@ -17,8 +17,8 @@ $this->params['breadcrumbs'][] = $this->title;
 use yii\bootstrap\Modal;
 Modal::begin([
     'id' => 'index-modal',
-//    'header' => '<h4 class="modal-title">认领产品</h4>',
     'footer' => '<a href="#" class="btn btn-primary" data-dismiss="modal">关闭</a>',
+    'size' => "modal-lg"
 ]);
 //echo
 Modal::end();
--- advanced/backend/views/oa-goods/view.php
+++ advanced/backend/views/oa-goods/view.php
@@ -11,16 +11,6 @@ $this->params['breadcrumbs'][] = ['label' => '产品推荐', 'url' => ['index']]
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="oa-goods-view">
-    <p>
-        <?= Html::a('更新', ['update', 'id' => $model->nid], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('删除', ['delete', 'id' => $model->nid], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
 
     <?= DetailView::widget([
         'model' => $model,
@@ -34,13 +24,13 @@ $this->params['breadcrumbs'][] = $this->title;
             'origin1',
             'origin2',
             'origin3',
-            'devNum',
-            'developer',
-            'introducer',
-            'devStatus',
-            'checkStatus',
-            'createDate',
-            'updateDate',
+//            'devNum',
+//            'developer',
+//            'introducer',
+//            'devStatus',
+//            'checkStatus',
+//            'createDate',
+//            'updateDate',
             'salePrice',
             'hopeWeight',
             'hopeRate',
