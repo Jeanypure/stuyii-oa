bca5907bda2ead12fdaebb99b334220b3e3c34ce
yxq <595551242@qq.com>
2017-12-25 17:45:18 +0800

    产品开发模块子目录获取修复

--- advanced/backend/views/oa-goods/update.php
+++ advanced/backend/views/oa-goods/update.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\OaGoods */
@@ -55,7 +56,7 @@ $this->registerJs($JS);
             [
                 'prompt'=>'--请选择父类--',
                 'onchange'=>'           
-            $.get("'.yii::$app->urlManager->createUrl('oa-goods/category').
+            $.get("'.Url::to(['oa-goods/category', 'typeid' => 1]).
                     '&typeid=1&pid="+$(this).val(),function(data){
                 var str="";
               $("select#oagoods-subcate").children("option").remove();
