0e7216fe04a02aa53f407d444b885708a8c895a0
turpure <p812816627@163.com>
2017-12-28 10:25:51 +0800

    correct filed name

--- advanced/backend/controllers/OaCheckController.php
+++ advanced/backend/controllers/OaCheckController.php
@@ -127,7 +127,7 @@ class OaCheckController extends Controller
             $_model->picUrl = $img;
             $_model->developer =$developer;
             $_model->devDatetime =strftime('%F %T');
-            $_model->updatTime =strftime('%F %T');
+            $_model->updateTime =strftime('%F %T');
             $_model->achieveStatus='待处理';
             $_model->GoodsName='';
             $arc_model = OaSysRules::find()->where(['ruleKey' => $developer])->andWhere(['ruleType' => 'dev-arc-map'])->one();
