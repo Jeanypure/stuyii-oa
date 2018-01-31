ed11f73b6a01cfb61587377a0c6e86cebfba14cd
Jeany <wakakajingjing@gmail.com>
2018-01-31 16:32:16 +0800

    delete salerTrend foreach

--- advanced/backend/controllers/SalesTrendController.php
+++ advanced/backend/controllers/SalesTrendController.php
@@ -59,41 +59,17 @@ class SalesTrendController extends \yii\web\Controller
             $arr_qty[] = $it;
             $arr_amt[] = $item;
         }
-        //处理name
-        //var_dump($name);exit;
-        $name_arr = [];
-        foreach ($name as $k => $v){
-            if($v == 'aliexpress'){
-                $name_arr[$k] = 'SMT';
-            }elseif ($v == 'amazon11'){
-                $name_arr[$k] = 'Amazon';
-            }elseif ($v == 'ebay'){
-                $name_arr[$k] = 'eBay';
-            }elseif ($v == 'joom'){
-                $name_arr[$k] = 'Joom';
-            }elseif ($v == 'lazada'){
-                $name_arr[$k] = 'Lazada';
-            }elseif ($v == 'shopee'){
-                $name_arr[$k] = 'Shopee';
-            }elseif ($v == 'topbuy'){
-                $name_arr[$k] = 'Topbuy';
-            }elseif ($v == 'wish'){
-                $name_arr[$k] = 'Wish';
-            }else{
-                $name_arr[$k] = $v;
-            }
-        }
+
         //获取销量数据
         $salesData = [
             'date' => $date,
-            'name' => $name_arr,
+            'name' => $name,
             'value' => $arr_qty
         ];
-        //var_dump($salesData);exit;
         //获取销售额数据
         $salesVolumeData = [
             'date' => $date,
-            'name' => $name_arr,
+            'name' => $name,
             'value' => $arr_amt
         ];
 
